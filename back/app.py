fce1019e28a7572a1745790323fa069887e75972
wkdtldn <matteo.jsw@gmail.com>
2023-06-17 15:55:36 +0900

    :memo:update

--- back/app.py
+++ back/app.py
@@ -1,29 +1,10 @@
-from flask import Flask, render_template
-import googletrans  
-
-# import googletrans  
-
-# translator = googletrans.Translator()
-
-# print("{번역기}")
-# while True:
-#     trans = input("한국어 검색(1)\n영어 검색(2)\n")
-#     if trans == "1":
-#         str1 = input("한국어 => 영어\n입력: ")
-#         result1 = translator.translate(str1,dest='en')
-#         print(f"%s => {result1.text}"%str1)
-#         print('-'*50)
-
-#     elif trans == "2":
-#         str2 = input("영어 => 한국어\n입력:")
-#         result2 = translator.translate(str2,dest='ko')
-#         print(f"%s => {result2.text}"%str2)
-#         print('-'*50)
-    
-#     else:
-#         break
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import googletrans
 
 app = Flask(__name__)
+
+CORS(app)
 
 translator = googletrans.Translator()
 
@@ -34,20 +15,22 @@ def index(methods=['GET', 'POST']):
 @app.route('/main')
 def main(methods=['GET', 'POST']):
     return "main"
-
-@app.route('/trans_ko')
+    
+@app.route('/trans_ko', methods=['GET', 'POST'])
 def trans__ko():
-    word1 = "안녕하세요"
-    result_ko = translator.translate(word1,dest='en')
-    return f"%s => {result_ko.text}"%word1
+    text = request.form['text']
+    lang = request.form['lang']
+    
+    res = translator.translate(text,dest='en')
+    return jsonify({'text': f"{res.text}", 'lang': lang})
 
-@app.route('/trans_en')
+@app.route('/trans_en', methods=['GET', 'POST'])
 def trans__en():
-    word2 = "hello"
-    result_en = translator.translate(word2,dest='ko')
-    return f"%s => {result_en.text}"%word2
-
-
+    
+    text = request.form['text']
+    lang = request.form['lang']
+    res = translator.translate(text,dest='ko')
+    return jsonify({'text': f"{res.text}", 'lang': lang})
     
 if __name__ == '__main__':
     app.run(debug=True)
